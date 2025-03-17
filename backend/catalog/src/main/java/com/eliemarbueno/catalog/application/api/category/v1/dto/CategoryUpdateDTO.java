f1537171d10b01a82bbaf067d1e717a333c6bbde
Eliemar Bueno <eliemarbueno@gmail.com>
2025-03-17 13:16:09 -0300

    Update contructor from dto.

--- backend/catalog/src/main/java/com/eliemarbueno/catalog/application/api/category/v1/dto/CategoryUpdateDTO.java
+++ backend/catalog/src/main/java/com/eliemarbueno/catalog/application/api/category/v1/dto/CategoryUpdateDTO.java
@@ -1,10 +1,21 @@
 package com.eliemarbueno.catalog.application.api.category.v1.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class CategoryUpdateDTO {
 
-	private String id;
-	private String name;
+  private String id;
+  private String name;
+
+  public CategoryUpdateDTO(String name) {
+    this.name = name;
+  }
 }
