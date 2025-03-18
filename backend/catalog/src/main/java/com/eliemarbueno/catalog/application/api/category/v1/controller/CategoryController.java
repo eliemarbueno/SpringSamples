b28705b1e1ac9fc9575df11b889ec953adef54ff
Eliemar Bueno <eliemarbueno@gmail.com>
2025-03-18 11:14:31 -0300

    Change name of method of delete.

--- backend/catalog/src/main/java/com/eliemarbueno/catalog/application/api/category/v1/controller/CategoryController.java
+++ backend/catalog/src/main/java/com/eliemarbueno/catalog/application/api/category/v1/controller/CategoryController.java
@@ -23,37 +23,47 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(ConstantsApiEndpoints.CATEGORY_V1)
 public class CategoryController {
 
-	private final CategoryService categoryService;
+  private final CategoryService categoryService;
 
-	public CategoryController(CategoryService categoryService) {
-		this.categoryService = categoryService;
-	}
+  public CategoryController(CategoryService categoryService) {
+    this.categoryService = categoryService;
+  }
 
-	@GetMapping
-	public Page<CategoryRetrieveDTO> findAllCategories(@RequestParam(required = false) String name, Pageable pageable) {
-		return categoryService.findAllCategories(name, pageable);
-	}
+  @GetMapping
+  public Page<CategoryRetrieveDTO> findAllCategories(
+    @RequestParam(required = false) String name,
+    Pageable pageable
+  ) {
+    return categoryService.findAllCategories(name, pageable);
+  }
 
-	@PostMapping
-	public ResponseEntity<CategoryRetrieveDTO> createCategory(@RequestBody CategoryCreateDTO categoryCreateDTO) {
-		var categorySaved = categoryService.create(categoryCreateDTO);
-		return ResponseEntity.status(HttpStatus.CREATED).body(categorySaved);
-	}
+  @PostMapping
+  public ResponseEntity<CategoryRetrieveDTO> createCategory(
+    @RequestBody CategoryCreateDTO categoryCreateDTO
+  ) {
+    var categorySaved = categoryService.create(categoryCreateDTO);
+    return ResponseEntity.status(HttpStatus.CREATED).body(categorySaved);
+  }
 
-	@GetMapping("/{id}")
-	public ResponseEntity<CategoryRetrieveDTO> getCategory(@PathVariable String id) {
-		return ResponseEntity.ok(categoryService.findById(id));
-	}
+  @GetMapping("/{id}")
+  public ResponseEntity<CategoryRetrieveDTO> getCategory(
+    @PathVariable String id
+  ) {
+    return ResponseEntity.ok(categoryService.findById(id));
+  }
 
-	@PutMapping("/{id}")
-	public ResponseEntity<CategoryRetrieveDTO> updateCategory(@PathVariable String id,
-			@RequestBody CategoryUpdateDTO categoryUpdateDTO) {
-		return ResponseEntity.ok(categoryService.update(id, categoryUpdateDTO));
-	}
+  @PutMapping("/{id}")
+  public ResponseEntity<CategoryRetrieveDTO> updateCategory(
+    @PathVariable String id,
+    @RequestBody CategoryUpdateDTO categoryUpdateDTO
+  ) {
+    return ResponseEntity.ok(categoryService.update(id, categoryUpdateDTO));
+  }
 
-	@DeleteMapping("/{id}")
-	public ResponseEntity<Void> deleteCategory(@PathVariable String id) {
-		return categoryService.deleteCategory(id) ? ResponseEntity.noContent().build()
-				: ResponseEntity.notFound().build();
-	}
+  @DeleteMapping("/{id}")
+  public ResponseEntity<Void> deleteCategory(@PathVariable String id) {
+    return categoryService.delete(id)
+      ? ResponseEntity.noContent().build()
+      : ResponseEntity.notFound().build();
+  }
 }
--- backend/catalog/src/main/java/com/eliemarbueno/catalog/domain/category/v1/service/CategoryService.java
+++ backend/catalog/src/main/java/com/eliemarbueno/catalog/domain/category/v1/service/CategoryService.java
@@ -1,9 +1,5 @@
 package com.eliemarbueno.catalog.domain.category.v1.service;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.stereotype.Service;
-
 import com.eliemarbueno.catalog.application.api.category.v1.dto.CategoryCreateDTO;
 import com.eliemarbueno.catalog.application.api.category.v1.dto.CategoryRetrieveDTO;
 import com.eliemarbueno.catalog.application.api.category.v1.dto.CategoryUpdateDTO;
@@ -11,76 +7,107 @@ import com.eliemarbueno.catalog.application.exception.v1.handler.ItemAlreadyExis
 import com.eliemarbueno.catalog.application.exception.v1.handler.ItemNotFoundException;
 import com.eliemarbueno.catalog.domain.category.v1.entity.Category;
 import com.eliemarbueno.catalog.domain.category.v1.repository.CategoryRepository;
-
 import jakarta.transaction.Transactional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
 
 @Service
 public class CategoryService {
 
-	private final CategoryRepository categoryRepository;
+  private final CategoryRepository categoryRepository;
 
-	public CategoryService(CategoryRepository categoryRepository) {
-		this.categoryRepository = categoryRepository;
-	}
+  public CategoryService(CategoryRepository categoryRepository) {
+    this.categoryRepository = categoryRepository;
+  }
 
-	@Transactional
-	public CategoryRetrieveDTO create(CategoryCreateDTO categoryDTO) {
-		var category = new Category(categoryDTO.name());
-		prePersist(category);
-		var categorySaved = categoryRepository.save(category);
+  @Transactional
+  public CategoryRetrieveDTO create(CategoryCreateDTO categoryDTO) {
+    var category = new Category(categoryDTO.name());
+    prePersist(category);
+    var categorySaved = categoryRepository.save(category);
 
-		return new CategoryRetrieveDTO(categorySaved.getId(), categorySaved.getName());
-	}
+    return new CategoryRetrieveDTO(
+      categorySaved.getId(),
+      categorySaved.getName()
+    );
+  }
 
-	public CategoryRetrieveDTO findById(String id) {
-		var category = categoryRepository.findById(id)
-				.orElseThrow(() -> new ItemNotFoundException("Category", "id", id));
-		return new CategoryRetrieveDTO(category.getId(), category.getName());
-	}
+  public CategoryRetrieveDTO findById(String id) {
+    var category = categoryRepository
+      .findById(id)
+      .orElseThrow(() -> new ItemNotFoundException("Category", "id", id));
+    return new CategoryRetrieveDTO(category.getId(), category.getName());
+  }
 
-	public Category getById(String id) {
-		return categoryRepository.findById(id).orElseThrow(() -> new ItemNotFoundException("Category", "id", id));
-	}
+  public Category getById(String id) {
+    return categoryRepository
+      .findById(id)
+      .orElseThrow(() -> new ItemNotFoundException("Category", "id", id));
+  }
 
-	@Transactional
-	public CategoryRetrieveDTO update(String id, CategoryUpdateDTO category) {
-		var existingCategory = getById(id);
-		existingCategory.setName(category.getName());
-		preUpdate(existingCategory);
-		var categorySaved = categoryRepository.save(existingCategory);
-		return new CategoryRetrieveDTO(categorySaved.getId(), categorySaved.getName());
-	}
+  @Transactional
+  public CategoryRetrieveDTO update(String id, CategoryUpdateDTO category) {
+    var existingCategory = getById(id);
+    existingCategory.setName(category.getName());
+    preUpdate(existingCategory);
+    var categorySaved = categoryRepository.save(existingCategory);
+    return new CategoryRetrieveDTO(
+      categorySaved.getId(),
+      categorySaved.getName()
+    );
+  }
 
-	@Transactional
-	public boolean deleteCategory(String id) {
-		var category = getById(id);
-		if (category == null) {
-			return false;
-		}
+  @Transactional
+  public boolean delete(String id) {
+    var category = getById(id);
+    if (category == null) {
+      return false;
+    }
 
-		categoryRepository.deleteById(id);
-		return true;
-	}
+    categoryRepository.deleteById(id);
+    return true;
+  }
 
-	public Page<CategoryRetrieveDTO> findAllCategories(String name, Pageable pageable) {
-		var page = categoryRepository.findAllByNameContainingIgnoreCase(name, pageable);
+  public Page<CategoryRetrieveDTO> findAllCategories(
+    String name,
+    Pageable pageable
+  ) {
+    var page = categoryRepository.findAllByNameContainingIgnoreCase(
+      name,
+      pageable
+    );
 
-		return page.map(this::toDTO);
-	}
+    return page.map(this::toDTO);
+  }
 
-	private CategoryRetrieveDTO toDTO(Category category) {
-		return new CategoryRetrieveDTO(category.getId(), category.getName());
-	}
+  private CategoryRetrieveDTO toDTO(Category category) {
+    return new CategoryRetrieveDTO(category.getId(), category.getName());
+  }
 
-	public void prePersist(Category category) {
-		if (categoryRepository.findByNameIgnoreCase(category.getName()).isPresent()) {
-			throw new ItemAlreadyExistsException("Category", "name", category.getName());
-		}
-	}
+  public void prePersist(Category category) {
+    if (
+      categoryRepository.findByNameIgnoreCase(category.getName()).isPresent()
+    ) {
+      throw new ItemAlreadyExistsException(
+        "Category",
+        "name",
+        category.getName()
+      );
+    }
+  }
 
-	public void preUpdate(Category category) {
-		if (categoryRepository.findByNameIgnoreCaseAndIdNot(category.getName(), category.getId()).isPresent()) {
-			throw new ItemAlreadyExistsException("Category", "name", category.getName());
-		}
-	}
+  public void preUpdate(Category category) {
+    if (
+      categoryRepository
+        .findByNameIgnoreCaseAndIdNot(category.getName(), category.getId())
+        .isPresent()
+    ) {
+      throw new ItemAlreadyExistsException(
+        "Category",
+        "name",
+        category.getName()
+      );
+    }
+  }
 }
